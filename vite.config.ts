42a70548dabb9bf96b553051173b877e26bf9202
Htet Aung Hlaing <ting.hah@pouchen.com>
2026-04-24 10:11:47 +0630

    Fix Vite base path and update index.html title

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/htetaunghlaing_portfolio/',
+    base: '/',
     plugins: [react(), tailwindcss({
 
     })],
